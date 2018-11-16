79068298aa37e2ec888c24913da4119749f06ac7
kieron wiltshire <kieron.wiltshire@outlook.com>
2018-11-16 20:03:56 +0000

    just need to actually code the damn thing now, where is the motivation? :P

--- src/mogront.js
+++ src/mogront.js
@@ -2,6 +2,7 @@
 'use strict';
 
 import fs from 'fs';
+import Monk from 'monk';
 import {default as createDebugger} from 'debug';
 
 const debug = createDebugger('mogront:bootstrap');
@@ -10,5 +11,29 @@ const debug = createDebugger('mogront:bootstrap');
  *
  */
 export default class Mogront {
+
+  /**
+   * Create a new {Mogront} instance.
+   */
+  constructor(monk, {
+    collectionName = 'mogront', // The name of the collection that will store migration state
+    migrationsDir = './migrations' // Relative path to the migrations directory
+  } = {}) {
+    if (monk instanceof Monk) {
+      this._monk = monk;
+    } else {
+      throw new Exception('The first argument needs to be an instance of {Monk}.')
+    }
+
+    collectionName = collectionName.toString();
+
+    if (collectionName.length > 0 && /^(?![0-9]*$)[a-zA-Z0-9]+$/.test(collectionName)) {
+      this._collectionName = collectionName;
+    } else {
+      throw new Exception('The specified collection name cannot conform to mongodb\'s specifications')
+    }
+
+    this._migrationsDir = migrationsDir;
+  }
 
 }
